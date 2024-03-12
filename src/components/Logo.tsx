6b35417b94d2850556d2031ff706432a2c57e688
Visibyte <nathanglass26@gmail.com>
2024-03-12 11:38:31 +0000

    logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Link href="/" className="flex gap-x-2">
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={50}
+        height={50}
+        className="cursor-pointer"
+        priority={true}
+      />
+      <span className="font-bold text-2xl bg-gradient-to-r from-teal-400 to-orange-400 dark:from-cyan-400 dark:to-orange-400 text-transparent bg-clip-text hover:cursor-pointer">
+        FormBuilder.io
+      </span>
+    </Link>
+  );
 }
 
 export default Logo;
